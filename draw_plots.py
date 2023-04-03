7a91aeb193569e30fb76301038a7584d5b35c983
darkhan-b <xxlabbesxx@gmail.com>
2023-04-04 03:12:32 +0800

    Upgrade full code

--- draw_plots.py
+++ draw_plots.py
@@ -1,48 +1,23 @@
-import pandas as pd  # import pandas and matplotlib
 import os
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class Plots:  # create class for plots
-    def __init__(self):
-        self.figure, self.axis1 = plt.subplots()  # constructor
-
-    def plot(self, x, y, label):
-        self.axis1.plot(x, y, label=label)
-
-    def set_title(self, title):  # setters
-        self.axis1.set_title(title)
-
-    def set_x_label(self, x_label):
-        self.axis1.set_xlabel(x_label)
-
-    def set_y_label(self, y_label):
-        self.axis1.set_ylabel(y_label)
-
-    def legend(self):
-        self.axis1.legend()  # legend info plot
-
-    def save_fig(self, filepath):  # save figure
-        self.figure.savefig(filepath)
+class Plots:
+    def draw_plots(self, file):
+        df = pd.read_json(file) # read json file
+        os.makedirs('plots', exist_ok=True) # create directory for plots
+        paths_return = [] # list of plots
 
 
-def draw_plots(json_file):
-    df = pd.read_json(json_file)  # parameter json from jupyter path and using pandas for read file
+        fig, ax = plt.subplots() # draw plots
+        ax.plot(df['min'], df['max']) # columns max and min
+        plt.savefig('plots/plot1.png')
+        paths_return.append('plots/plot1.png')
 
-    if not os.path.exists('plots'):
-        os.makedirs('plots')  # create folder "plots"
+        fig, ax = plt.subplots()
+        ax.plot(df['gt_corners'], df['rb_corners']) # columnts gt_corners and rb coreners
+        plt.savefig('plots/plot2.png') #savefig from class
+        paths_return.append('plots/plot2.png')
 
-    plot_compare = Plots()  # Initialize the plotCompare from class
-
-    for col in df.columns:
-        plot_compare.plot(df.index, df[col], label=col)  # comparing each plot using loop columns
-
-    plot_compare.set_title('Comparison Columns')
-    plot_compare.set_x_label('Indexes from deviations')  # titles
-    plot_compare.set_y_label('General value comparing')
-    plot_compare.legend()
-
-    filepath = os.path.join('plots', 'testPlot.png')  # using join for all plots to filepath
-    plot_compare.save_fig(filepath)  # saving plot
-
-    return filepath  # return the path to the plot
+        return paths_return # return to Jupyter output
---
+++ test.py
@@ -0,0 +1,48 @@
+import json
+import unittest
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from draw_plots import Plots # import class Plots
+
+class TestPlots(unittest.TestCase): # create test plot class using unittest
+    def setUp(self):
+        self.plot_out = Plots()
+        self.test_json_file = 'deviation.json' # json file for unittest
+        self.paths = self.plot_out.draw_plots(self.test_json_file)
+
+    def pathRemove(self):
+        for path in self.paths:
+            os.remove(path) # remove path
+
+    def test_draw_plots(self):
+        self.assertTrue(os.path.exists('plots/plot1.png'))
+        self.assertTrue(os.path.exists('plots/plot2.png'))
+
+    def test_return_type(self): # assertIsInstance returning data
+        self.assertIsInstance(self.paths, list)
+        for path in self.paths:
+            self.assertIsInstance(path, str)
+
+    def test_plot_contents(self): # load data from json into df for data response to plot
+        with open(self.test_json_file, 'r') as f:
+            data = json.load(f)
+        df = pd.DataFrame.from_dict(data) # data Dataframe read
+
+        # Check that the first plot has the correct data
+        fig, ax = plt.subplots()
+        ax.plot(df['min'], df['max']) # columns
+        expected_path = 'plots/plot1.png' # plot path
+        actual_path = self.paths[0] # index
+        self.assertTrue(os.path.exists(expected_path))
+        self.assertTrue(os.path.exists(actual_path))
+        self.assertEqual(open(expected_path, 'rb').read(), open(actual_path, 'rb').read()) # read data
+
+        # Check that the second plot has the correct data
+        fig, ax = plt.subplots()
+        ax.plot(df['gt_corners'], df['rb_corners']) # columns
+        expected_path = 'plots/plot2.png' # plot path
+        actual_path = self.paths[1] # index
+        self.assertTrue(os.path.exists(expected_path))
+        self.assertTrue(os.path.exists(actual_path))
+        self.assertEqual(open(expected_path, 'rb').read(), open(actual_path, 'rb').read()) # read data
